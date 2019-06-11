353cbab2fbc9ea0f4adc097b111b193a9dc78948
Sansob <hasan.shobri@outlook.com>
2019-06-11 16:21:54 +0700

    Update new UI

---
+++ LeaveApplication/UserInterfaceLeaveApp/DashboardUserWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LeaveApplication.UserInterfaceLeaveApp {
+    /// <summary>
+    /// Interaction logic for DashboardUserWindow.xaml
+    /// </summary>
+    public partial class DashboardUserWindow : Window {
+        public DashboardUserWindow() {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e) {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e) {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+    }
+}
--- LeaveApplication/UserInterfaceLeaveApp/LoginWindow.xaml.cs
+++ LeaveApplication/UserInterfaceLeaveApp/LoginWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LeaveApplication {
     /// <summary>
@@ -21,7 +9,6 @@ namespace LeaveApplication {
         public MainWindow() {
             InitializeComponent();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e) {
 
         }
