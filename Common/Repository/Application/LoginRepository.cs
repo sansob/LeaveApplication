bb38c2a1a1e654d3817abfa3adabe8694571ede3
Gian <gianpetriciaaurora@gmail.com>
2019-06-13 08:06:36 +0700

    Login

---
+++ Common/Repository/Application/LoginRepository.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.MyContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository.Application
+{
+    public class LoginRepository
+    {
+        MyContext myContext = new MyContext();
+        public bool cekLogin(string userName, string password)
+        {
+            bool cek = false;
+            var temp = myContext.employees.Where(x => x.UserName == userName && x.Password == password).SingleOrDefault();
+
+            try
+            {
+                if(temp.UserName == userName && temp.Password == password)
+                {
+                    cek = true;
+                }
+                else
+                {
+                    cek = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                cek = false;
+                ex.GetBaseException();
+            }
+            return cek;
+        }
+    }
+}
---
+++ LeaveApplication/UserInterfaceLeaveApp/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Common.Repository.Application;
+using DataAccess.MyContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LeaveApplication.UserInterfaceLeaveApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        MyContext myContext = new MyContext();
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            LoginRepository login = new LoginRepository(); 
+            if(login.cekLogin(username.Text, password.Password) == true)
+            {
+                MessageBox.Show("Login Berhasil", "Peringatan", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Hide();
+                DashboardUserWindow home = new DashboardUserWindow();
+                home.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Login Gagal", "Peringatan", MessageBoxButton.OK, MessageBoxImage.Information);
+                password.Clear();
+                password.Focus();
+            }
+        }
+    }
+}
