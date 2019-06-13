bcef88442aed8362676eaa60a58d8c6c7e5dc446
Sansob <hasan.shobri@outlook.com>
2019-06-14 01:18:12 +0700

    Update Delete
    Update Delete

--- LeaveApplication/UserInterfaceLeaveApp/EditLeaveWindow.xaml.cs
+++ LeaveApplication/UserInterfaceLeaveApp/EditLeaveWindow.xaml.cs
@@ -19,6 +19,28 @@ namespace LeaveApplication.UserInterfaceLeaveApp {
         private int _fromRequestId;
         private string _reqName, _reqDate, _reqDateEnd, _reqLeaveType, _reqRequestDate, _reqStatusRequest, _reqManager, _reqHrd, _reqReason;
 
+        private void Button_Click_1(object sender, RoutedEventArgs e) {
+            leaveRequestVM.Id = Convert.ToInt32(_fromRequestId);
+            var setStatus = iLeaveRequestService.SetStatus(leaveRequestVM.Id, leaveRequestVM.Status_Id = Convert.ToInt32(7));
+            if (setStatus) {
+                MessageBox.Show("Rejected");
+                Button_Approve.Visibility = Visibility.Collapsed;
+                Button_Reject.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e) {
+            var dialogResult = MessageBox.Show("Are you sure?", "Delete Confirmation", MessageBoxButton.YesNo).ToString();
+            if (dialogResult == "Yes") {
+                var result = iLeaveRequestService.Delete(Convert.ToInt32(_fromRequestId));
+                if (result) {
+                    MessageBox.Show("Delete Successfully");
+                } else {
+                    MessageBox.Show("Delete Fail");
+                }
+            }
+        }
+
         LeaveRequestVM leaveRequestVM = new LeaveRequestVM();
         LeaveTypeVM leaveTypeVM = new LeaveTypeVM();
 
@@ -86,11 +108,9 @@ namespace LeaveApplication.UserInterfaceLeaveApp {
             cmb_LeaveType.ItemsSource = iLeaveTypeService.Get();
             if (_employee.Role == "1") {
                 Button_Approve.Visibility = Visibility.Collapsed;
+                Button_Reject.Visibility = Visibility.Collapsed;
             }
             else if (_employee.Role == "2") {
-
-
-
                 Button_Approve.Visibility = Visibility.Visible;
             } else if (_employee.Role == "3") {
                 Button_Approve.Visibility = Visibility.Visible;
